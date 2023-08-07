db46f6325c5598e93e403782513dc0d7e25ea295
moyuboo1 <moyuboo1@keio.jp>
2023-08-07 18:01:09 +0900

    ADD: Coordinate Publisher

--- cal_rtheta/input.py
+++ cal_rtheta/input.py
@@ -1,40 +1,34 @@
+import os
+import sys
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Point
 
 class Input(Node):
     def __init__(self):
         super().__init__('input')
-        self.publisher_ = self.create_publisher(Float32MultiArray, 'input', 10)
-        self.tmr = self.create_timer(0.1, self.callback)
-        self.xy=[0.0,0.0]
-        
-    
-    def callback(self,x,y):
-        self.xy[0]=x
-        self.xy[1]=y
-        input_data = Float32MultiArray()
-        input_data.data = self.xy
-        self.pos_publisher.publish(input_data)
+        self.publisher_ = self.create_publisher(Point, 'input', 1)
+        self.timer_period = 5.0  # メッセージを送信する間隔（秒）
+        self.timer = self.create_timer(self.timer_period, self.publish_coordinates)
+
+    def publish_coordinates(self):
+        # ここで座標を読み込むか、直接座標を生成するか決定します
+        # この例では、座標を直接生成します
+        coordinates = [
+            (0.4, 0.0),
+        ]
+
+        for coord in coordinates:
+            msg = Point()
+            msg.x, msg.y= coord
+            self.publisher_.publish(msg)
+            self.get_logger().info(f"Published coordinate: {msg.x}, {msg.y}")
 
 def main(args=None):
     rclpy.init(args=args)
-    input_publisher = Input()
-
-    while rclpy.ok():
-        try:
-            x = float(input('Enter x coordinate: '))
-            y = float(input('Enter y coordinate: '))
-        except ValueError:
-            print('Invalid input! Please enter numerical values.')
-            continue
-        
-        input_publisher.callback(x, y)
-
-    input_publisher.destroy_node()
+    node = Input()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
-
--- cal_rtheta/xy_to_rtheta.py
+++ cal_rtheta/xy_to_rtheta.py
@@ -3,13 +3,14 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import math
 import numpy as np
+from geometry_msgs.msg import Point
 
 class XY_to_Rtheta(Node):
     def __init__(self):
         super().__init__('xy_to_rtheta')
         self.pos_publisher = self.create_publisher(Float32MultiArray, 'pos_data', 10)
         #self.degpos_publisher = self.create_publisher(Float32MultiArray, 'degpos_data', 10)
-        #self.subscription = self.create_subscription(Float32MultiArray, 'joy_data', self.joy_callback, 10)
+        self.subscription = self.create_subscription(Point, 'input', self.input_callback, 1)
         self.tmr = self.create_timer(0.1, self.callback)
         self.status = 0
         self.r = 0.0
@@ -25,11 +26,13 @@ class XY_to_Rtheta(Node):
         self.init = 0.0
         self.currentPos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.degPos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        self.x=0.43
-        self.y=0.43
+        self.x=0.0
+        self.y=0.0
 
-    
-    def cal(self):
+    def input_callback(self,input_msg):
+        self.x=input_msg.x
+        self.y=input_msg.y
+
         self.currentPos[0]=math.atan2(self.y,self.x)
         self.currentPos[1]=math.sqrt(self.x**2+self.y**2)
         self.currentPos[2]=0.0
@@ -39,7 +42,6 @@ class XY_to_Rtheta(Node):
         self.currentPos[6]=0.0
 
     def callback(self):
-        self.cal()
         pos_data = Float32MultiArray()
         pos_data.data = self.currentPos
         self.pos_publisher.publish(pos_data)
